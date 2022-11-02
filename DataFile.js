b15c873a23b08aff9b30891e6806765a24fc3df0
bxhsbshheusv <117252207+bxhsbshheusv@users.noreply.github.com>
2022-11-02 14:03:01 +0300

    Update DataFile.js

--- DataFile.js
+++ DataFile.js
@@ -14,34 +14,34 @@ global.APIKeys = {
 	'https://zenzapis.xyz': 'sanzychan01', //credit goes to sanzy yt, if api key expire, buy it and put it here?\\
 }
 // 👈 You Can change this your choice
-global.emoji01 = ['🐼'] // 👈 Enter your Emoji 
+global.emoji01 = ['😍'] // 👈 Enter your Emoji 
 global.symb = '█' // 👈  Enter Any ▁ ▂ █ ░ ▘ □ ▣
 global.symb2 = '▎ █' // 👈 Enter Any ▬▓▌▎■
 global.symb3 = '▬' // 👈 Enter Any ▅ ▂ ▁ ▊
-global.emoji02 = ['🦋'] //👈 Enter your Emoji
-global.notice = ` *Hi 🥰 I m*
+global.emoji02 = ['🐈'] //👈 Enter your Emoji
+global.notice = ` *مرحبا 🥰 I m*
 *ＮＯＩＺ Ξ*
-*I am 16 years old developer...*
-*From Sri Lanka 🇱🇰*
-*I am a Learning.*
-*Not more..*
-*Also I am Student*
+*انا اسمي شهاب ولقبي انوس عمري 16سنه...*
+*اعيش في اليمن 🇾🇪*
+*طالب في اثنويه.*
+*لاستو مبرمج ولاكن اتابع تطور البوتات 😜..*
+*اذا كنت تحب اتعرف عليا هذا رقمي +967771160204 $$+697730348571 *
 *Follow My Facebook - https://www.facebook.com/zenoixnoize*`  // 👈 Enter Your details and Social Media Links and Anything
-global.owner =  ['+94711502119'] //👈  Enter Your number
-global.premium =  ['+94711502119'] //👈  Enter Your number
-global.ownernomer = '+94711502119' //👈  Enter Your number
-global.ownername = 'ZENOI' //👈 Enter Your name
-global.botname = 'ＧＡＲＦＩＥＬＤ ＢＯＴ' //👈 Enter Your Bot name
+global.owner =  ['+967771160204 '] //👈  Enter Your number
+global.premium =  ['+697730348571 '] //👈  Enter Your number
+global.ownernomer = '+697730348571' //👈  Enter Your number
+global.ownername = 'الامبراطور انوس ' //👈 Enter Your name
+global.botname = '「⚜️」𒆜ËMPËŖÖŖ ÄŅŮŚ' //👈 Enter Your Bot name
 global.footer = '© 𝖯𝗈𝗐𝖾𝗋𝖾𝖽 𝖡𝗒 𝖦𝖺𝗋𝖿𝗂𝖾𝗅𝖽 𝖡𝗈𝗍' //👈 Enter Your Name
-global.ig = 'https://github.com/Zenoixnoize/GARFIELD-WHATSAPP-BOT-v8' // 👈 You Can change this your choice
+global.ig = 'https://chat.whatsapp.com/JUngwIYTMVzCxMuismJuOY' // 👈 You Can change this your choice
 global.region = 'India, South India, Kerala' // 👈 You Can change this your choice
-global.sc = 'https://github.com/Zenoixnoize/GARFIELD-WHATSAPP-BOT-v8'
-global.fbt = 'Follw Us ℹ️' // 👈 Enter (Subscribe/Follow/Like now)
+global.sc ='https://youtu.be/nesSv2Gbh9s'
+global.fbt = 'التالي ℹ️' // 👈 Enter (Subscribe/Follow/Like now)
 global.myweb = 'https://www.facebook.com/garfieldbots/'// 👈 Enter your Social media link to follow now button
-global.welcome = '*Welcome To The Group 🦋*' // 👈 You Can change this your choice
-global.bye = '*Bye bye Mother fucker 🐼*' // 👈 You Can change this your choice
-global.packname = 'GARFIELD-BOTv8'  // 👈 You Can change this your choice 
-global.author = 'NOIZE DEVELOPMENT' // 👈 You Can change this your choice 
+global.welcome = '*الامبراطور انوس 🦋*' // 👈 You Can change this your choice
+global.bye = '*「⚜️」𒆜ËMPËŖÖŖ ÄŅŮŚ 🐼*' // 👈 You Can change this your choice
+global.packname = '「⚜️」𒆜ËMPËŖÖŖ ÄŅŮŚ'  // 👈 You Can change this your choice 
+global.author = '「⚜️」𒆜ËMPËŖÖŖ ÄŅŮŚ' // 👈 You Can change this your choice 
 global.sessionName = 'session' // 👈 You Can change this your choice
 global.prefa = ['','!','.','🛰️','⚓','🍎'] // 👈 You Can change this your choice
 //Go to https://imgbb.com/, upload a photo of your choice and put its link here
@@ -99,35 +99,35 @@ fs.watchFile(file, () => {
 
 
 // Update Logs
-// New Menu Ui
+// الاوامر
 /*
 ▬
-▎ Hi  Ｚ Ξ ＮＯＩ 👋 
-▎ ＧＡＲＦＩＥＬＤ ＢＯＴ
+▎ مرحبا بك Ｚ Ξ ＮＯＩ 👋 
+▎ 「⚜️」𒆜ËMPËŖÖŖ ÄŅŮŚ
 ▎ █ 𝗦𝗽𝗲𝗲𝗱 : 0.00119 miliseconds
 ▎ █ 𝗥𝘂𝗻𝘁𝗶𝗺𝗲 : 9 hours, 58 minutes, 23 seconds
-▎ █ 𝗕𝗼𝘁 𝗡𝗮𝗺𝗲 : ＧＡＲＦＩＥＬＤ ＢＯＴ 
-▎ █ 𝗢𝘄𝗻𝗲𝗿 𝗡𝗮𝗺𝗲 : ZENOI
-▎ █ 𝗢𝘄𝗻𝗲𝗿 𝗡𝘂𝗺𝗯𝗲𝗿 : +94711502119
-▎ █ 𝗛𝗼𝘀𝘁 𝗡𝗮𝗺𝗲 : ZENOI
-▎ █ 𝗣𝗹𝗮𝘁𝗳𝗼𝗿𝗺 : linux
-▎ █ 𝗧𝗼𝘁𝗮𝗹 𝗨𝘀𝗲𝗿 : 11
-   Ｍｅｎｕ Ｃｏｍｍａｎｄｓ🌀
+▎ █ مطور البوتات : 「⚜️」𒆜ËMPËŖÖŖ ÄŅŮŚ 
+▎ █ الامبراطور انوس: اسم مطور البوت 
+▎ █ +967771160204   
+▎ █ الأمبراطور انوس: اسم البوت 
+▎ █ معلونات : linux
+▎ █ 11:تشغيل البوت
+   قائمة الاوامر🌀
    ▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁ 
-   *▓  AllMenu* 
-   *▓  DownloadMenu*
-   *▓  Searchmenu*
-   *▓  mainimenu*
-   *▓  Convertmenu*
-   *▓  Funmenu*
-   *▓  Databasemenu*
-   *▓  Gamemenu*
+   *▓  الاوامر * 
+   *▓  الاباحي *
+   *▓  قائمة البحث*
+   *▓  القائمة الرئيسية *
+   *▓  قائمة التحويلات *
+   *▓  قائمة الهنتاي😜🤭😂*
+   *▓  قائمة البيانات *
+   *▓  قائمة الامبراطور انوس *
    
-▎ ️ＧＡＲＦＩＥＬＤ ＢＯＴ  Created by ZENOI  🪁
-▎ ᴘᴏᴡᴇʀᴇᴅ ʙʏ 𝖦Λ𝖱𝖥𝖨Ξ𝖫𝖣 𝖡𝖮Т v8.6 and 
+▎ ️「⚜️」𒆜ËMPËŖÖŖ ÄŅŮŚ  مرحبا بك 「⚜️」𒆜ËMPËŖÖŖ ÄŅŮŚ  🪁
+▎ ᴘᴏᴡᴇʀᴇᴅ ʙʏ الامبراطور انوس 𝖡𝖮Т v8.6 and 
 ▎ 𝖭Ξ𝖴𝖱Λ𝖫 ΛＩ v1.00             
 ⭕►▁▁▁▁▁▁▁▁▁▁▁▁▁▁
-   ▎ ＧＡＲＦＩＥＬＤ ＢＯＴ
+   ▎ حاكم اشياطين الامبراطور انوس 
    ▁▁▁▁▁▁▁▁▁▁▁▁▁▁
  ▁▁▁▁▁▁▁▁▁▁▁▁▁▁
 ▎ 
